e6f8d9edf37791c6e508e45db82b4f2c09bcfff1
dishavanani13 <dishvanani13@gmail.com>
2024-09-25 21:52:21 -0400

    Initial Commit

---
+++ DoDaily/DoDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoDailyApp.swift
+//  DoDaily
+//
+//  Created by Disha Limbani on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct DoDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
